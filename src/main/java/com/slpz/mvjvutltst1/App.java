a3391bd330006a6869720f66e06afce5b03bda4e
sebastian <svm4apps@gmail.com>
2014-03-11 11:16:47 -0400

    first commit test project

---
+++ src/main/java/com/slpz/mvjvutltst1/App.java
@@ -0,0 +1,13 @@
+package com.slpz.mvjvutltst1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello test1!" );
+    }
+}
